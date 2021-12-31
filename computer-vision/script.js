9bfb5d35b6045249ef44eeeb2126a06cf54106a8
nabilx03 <nabil.salama@outlook.de>
2021-12-31 22:08:02 +0100

    adding comming soon message

--- computer-vision/script.js
+++ computer-vision/script.js
@@ -1,7 +1,12 @@
 const tiles = {
     title: ["Library Construction", "AI model", "Training approach", "Results"],
     img: ['construction.png', 'construction.png', 'construction.png', 'construction.png'],
-    description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+    description: [
+        "The main idea of the library is to have levels, which contain layers. These are: Convolutional-, Dense-, Pooling- and Flatten Layers. There is also a \"Multi Dense-Layer\", that is designed for higher efficiency regarding space complexity, but less performance (see <a href='./../research'>Research</a>). Each layer's input is an output of a layer from one of the previos levels. This allows a construction of parallel layers like in the inception modules and the use of residuals.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    ]
 };
 
 const init = () => {
@@ -11,6 +16,7 @@ const init = () => {
                 <div>
                     <h3>${tiles.title[i]}</h3>
                     <p>${tiles.description[i]}</p>
+                    
                 </div>
                 <img src='./img/${tiles.img[i]}' />
             </div>
