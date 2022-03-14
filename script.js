34829ef7855e5831c68e2cf9e23ad25c13f5e0d8
nabil-2 <nabil.salama@outlook.de>
2022-03-14 15:35:14 +0100

    commit

--- script.js
+++ script.js
@@ -1,111 +1,207 @@
-const getProjects = ()=> {
-    return `[
-        {
-            "name": "Computer Vision Library<br>& Image Recognition",
-            "img": "computer_vision.png",
-            "url": "computer-vision",
-            "description": "A library for training and testing a user-definded Convolutional Neuronal Network model, capable of all common techniques for supervised image classification and image preprocessing."
-        },
-        {
-            "name": "AI for Anomaly Detection",
-            "img": "ai_anomaly_detection.png",
-            "url": "anomaly-detection",
-            "description": "An AI for anomaly detection in particle physics to recognize possible beyond Standard Model events when colliding hadrons (currently working on)."
-        },
-        {
-            "name": "Research",
-            "img": "research.png",
-            "url": "research",
-            "description": "Developing a new type of a storage space efficient neural network by decreasing unnecessary performance by calculating weights at runtime using a “weight function” instead of saving them. Achieving about 90% less storage and 10% higher error (depending on model)."
-        },
-        {
-            "name": "Simulation of a Pandemic",
-            "img": "simulation.png",
-            "url": "simulation",
-            "description": "A complex reconstruction of a pandemic as Web-App using real-world actions measured by comparing plots to mathematical models."
-        },
-        {
-            "name": "Android App",
-            "img": "android.jpg",
-            "url": "android-app",
-            "description": "A calculator for various linear- and vector algebra operations, e.g. calculating a linear combination in n-dimensional space, vector operations, calculating the determinant, etc."
-        },
-        {
-            "name": "Web Development",
-            "img": "web_dev.png",
-            "url": "http://salama-art.de/en",
-            "description": "Developing a <a href='http://salama-art.de/en'>personal website</a> (see footer) or a website for a <a href='./newspaper'>school newspaper</a>."
+const web = {
+    init() {
+        web.api = new apiC;
+        web.handleApp();
+        web.redraw();
+        $(".ctrl i:first-child").on("click", ()=> {
+            alert("Die Berechnungen können abhängig von Rechenleistung und Bevölkerungszahl einen Moment dauern.");
+            $(".loading").css({display: "block"});
+            web.asnycCall(web.start, web.redraw).then(()=> {
+                $(".loading").css({display: "none"});
+            });
+        });
+    },
+    async asnycCall(fct, arg) {
+        await fct(arg);
+    },
+    start(callback) {
+        return new Promise((resolve, reject)=> {
+            callback();
+            resolve();
+        });
+    },
+    api: null,
+    handleSettings() {  
+        web.readMeasures();
+        web.api.population = $("#populationVal").val();
+        web.api.startInfections = $("#startIval").val();
+        let i=0;
+        for(it in web.api.probabilities) {
+            let $inputs = $(".probabilities input");
+            web.api.probabilities[it].m = $($inputs.get(i)).val();
+            i++;
         }
-    ]`;
-    return new Promise((resolve, reject)=> {
-        var request = new XMLHttpRequest();
-        request.open("GET","http://nabils.bplaced.net/projects.json");
-        request.addEventListener('load', e=> {
-           if (request.status >= 200 && request.status < 300) {
-              resolve(request.responseText);
-           } else {
-              reject(request.statusText, request.responseText);
-           }
+        i=0;
+        for(it in web.api.values) {
+            let $inputs = $("#avgInt input");
+            web.api.values[it] = $($inputs.get(i)).val();
+            i++;
+        }
+    },
+    measures: [],
+    readMeasures() {
+        web.measures = [];
+        $(".measures table tr:not(:first-child)").each((ix, el)=>{
+            let day = $(el).children("td:nth-child(1)").children("input#measureDay").val();
+            let measure/*  = $(el).children("td:nth-child(2)").children("select#measurement").find(":selected").val() */,
+                measureGer = $(el).children("td:nth-child(2)").children("#measurement").find(":selected").text();
+            let status = $(el).children("td:nth-child(3)").children("input#measureStatus").is(":checked");
+            switch(measureGer) {
+                case "Lockdown":
+                    measure = "lockdown";
+                    break;
+                case "Maskenpflicht":
+                    measure = "facemask";
+                    break;
+                case "Abstandsregelung":
+                    measure = "socialDistance";
+                    break;
+                case "Reiseverbot":
+                    measure = "travelingProhibition";
+                    break;
+            }
+            //let s = status=="on";
+            web.measures.push([measureGer, parseInt(day), measure, status]);
         });
-        request.send();
-    });    
-};
-
-const init = async ()=> {
-    const projects = JSON.parse(await getProjects());
-    for(const project of projects) {
-        $('body #tiles').append(`<div class="project">
-            <h3>${project.name}</h3>
-            <p>${project.description}</p>
-        </div>`);        
-        let height = $('div.project:last-child p').height();
-        $('#tiles div.project:last-child').css({
-            'background-image': `url('./img/${project.img}')`
-        }).on('click', x=> {
-            $('body').append(`<a id="redirect" href="${project.url}" />`);
-            $('#redirect').get(0).click();
-        }).children('p').css({
-            height: 0,
-            "font-size": 0,
-            opacity: 0,
-            display: 'none'
-        }).parent('div.project').hover(function() {
-            $(this).children('p').css({
-                display: 'block',
-                "font-size": "1.35em"
-            }).animate({
-                height: height
-            }, {
-                duration: 400,
-                easing: 'swing',
-                complete: function() {
-                    $(this).animate({
-                        opacity: 1
-                    }, 300);
-                }
+    },
+    setDefaults() {
+        let tmp = [];
+        for(let i=0; i<web.measures.length; i++) {
+            tmp.push([]);
+            for(let j=0; j<web.measures[i].length; j++) {
+                tmp[i].push(web.measures[i][j]);
+            }
+        }
+        tmp.forEach((el, ix, arr)=> {
+            tmp[ix].splice(0, 1);
+        });
+        web.api.measuresPlan = tmp;
+        web.api.init();
+        $(".overallI").text(web.api.overallInfections);
+        $(".infectionRoot table tr").each(function(ix, el) {
+            $(this).children("td:last-child").html(web.api.infectionRoot[ix]);
+        });
+        let template = `<tr>
+            <th>Tag</th>
+            <th>S</th>
+            <th>I</th>
+            <th>R</th>
+            <th>Q</th>
+            <th>C</th>
+        </tr>`;
+        web.api.stats.forEach((el, ix, arr)=> {
+            template += "<tr>";
+            for(let val in el) {
+                template += `<td>${el[val]}</td>`;
+            }
+            template += "</tr>"
+            $(".furtherStats tbody").html(template);
+        });
+    },
+    handleApp() {
+        $(".ctrl i:last-child").on("click", ()=> {
+            location.reload();
+        });
+        $("input#population").on('change mousemove', function() {
+            $("input#populationVal").val($(this).val());
+        });
+        $("input#populationVal").on('change', function() {
+            $("input#population").val($(this).val());
+        });
+        $("input#startI").on('change mousemove', function() {
+            $("input#startIval").val($(this).val());
+        });
+        $("input#startIval").on('change', function() {
+            $("input#startI").val($(this).val());
+        });
+        $("#addMeasure").on("click", ()=> {
+            $(".measures table tbody").append(`
+            <tr>
+                <td><input type="number" id="measureDay" min="1" value="1" /></td>
+                <td><select id="measurement">
+                    <option>Lockdown</option>
+                    <option>Maskenpflicht</option>
+                    <option>Abstandsregelung</option>
+                    <option>Reiseverbot</option>
+                </select></td>
+                <td><input type="checkbox" id="measureStatus" checked="checked"/></td>
+                <td><img src="delete.jpg" /></td>
+            </tr>                
+            `);
+            $($("div.measures tr").get(1)).css({
+                "background-color": "white"
             });
-        }, function() {
-            $(this).children('p').animate({
-                opacity: 0
-            }, {
-                duration: 300,
-                complete: function() {
-                    $(this).animate({
-                        height: 0
-                    }, {
-                        duration: 400,
-                        complete: function() {
-                            $(this).css({
-                                display: 'none',
-                                "font-size": 0
-                            });
+            $(".measures img").on("click", function() {
+                $(this).parent().parent().remove();
+            });
+        });
+        $(".measures img").on("click", function() {
+            $(this).parent().parent().remove();
+            $("div.measures tr:nth-child(even)").css({
+                "background-color": "#dddddd"
+            });
+        });
+        $("#populationVal").val(web.api.population);
+        $("#population").val(web.api.population);
+        $("#startIval").val(web.api.startInfections);
+        $("#startI").val(web.api.startInfections);
+        let i=0;
+        for(it in web.api.probabilities) {
+            let el = web.api.probabilities[it];
+            let $inputs = $(".probabilities input");
+            $($inputs.get(i)).val(el.m);
+            i++;
+        }
+        i=0;
+        for(it in web.api.values) {
+            let $inputs = $("#avgInt input");
+            $($inputs.get(i)).val(web.api.values[it]);
+            i++;
+        }
+    },
+    redraw() {
+        web.api = new apiC();
+        web.handleSettings();
+        web.setDefaults();
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+            let data = new google.visualization.DataTable();
+            data.addColumn('string', 'x');
+            data.addColumn({type: 'string', role: 'annotation'});
+            data.addColumn({type: 'string', role: 'annotationText'});
+            data.addColumn('number', 'Infektionen');
+            //data.addColumn('number', 'Genesungen/Tode');
+            data.addColumn('number', 'Quarantänisierte');
+            //data.addRow(["G", 'Foo', 'Foo annotation', 8, 1, 0.5]);
+            web.api.stats.forEach((el, ix, arr)=> {
+                let line1 = null, line2 = null;
+                let events = "";
+                web.measures.forEach((el2, ix, arr)=> {
+                    if(el.day == el2[1]) {
+                        events += el2[0];
+                        line1 = el2[0];
+                        if(!el2[3]) {
+                            events += " aufgehoben";
+                            line1 += " aufgehoben";
                         }
-                    });
-                }
+                        events += ", ";
+                        line2 = "Tag " + el.day + ": " + events;
+                    }
+                });
+                if(line2) line2 = line2.substring(0, line2.length-2);
+                data.addRow([el.day.toString(), line1, line2, el.infections/*, el.recovered*/, el.quarantine]);
             });
-        });
+            var options = {
+                title: 'Pandemie-Entwicklung',
+                hAxis: {title: 'Tag',  titleTextStyle: {color: '#333'}},
+                vAxis: {minValue: 0},
+                annotations: {
+                    style: 'line'
+                }
+            };
+            var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        }
     }
-    if(screen.width <= 800) {
-        alert("This website is not optimized for mobile viewports, please use a desktop device.");
-    }
-};
+}
